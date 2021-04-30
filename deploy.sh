f1cd33ba566d7f35a33203bae9f6fa712d78e6ff
gao_yun(高赟) <gao_yun@winning.com.cn>
2021-04-30 15:48:27 +0800

    配置更改

--- deploy.sh
+++ deploy.sh
@@ -17,7 +17,7 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
- git push -f git@github.com:felicity-gao/felicity-gao.github.io.git master
+ git push -f git@github.com:felicitygao/felicitygao.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 #git push -f git@github.com:felicity-gao/Blog_view.git master
